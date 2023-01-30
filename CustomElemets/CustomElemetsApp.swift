7277232882c9a7bdaf21477885b9f10b279569a1
Dmitrii Lipuntsov <dmitriilipuntsov1@yandex.ru>
2023-01-30 16:48:16 +0800

    initial commit

---
+++ CustomElemets/CustomElemetsApp.swift
@@ -0,0 +1,17 @@
+//
+//  CustomElemetsApp.swift
+//  CustomElemets
+//
+//  Created by Dmitry Lipuntsov on 30.01.2023.
+//
+
+import SwiftUI
+
+@main
+struct CustomElemetsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
